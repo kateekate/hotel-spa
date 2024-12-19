353026fc66c743b372614dc454a23b999c9c0e10
Kate <kate@Arturs-MacBook-Pro.local>
2024-12-19 04:39:06 +0100

    refactor: update HotelsList layout and add ratings to HotelCard

--- src/pages/HotelsList.tsx
+++ src/pages/HotelsList.tsx
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 import { HotelCard } from "../components/HotelCard";
 
-const Container = styled.div`
+const List = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   gap: 16px;
+  height: 100vh;
   padding: 20px;
+  background-color: #e8e6e1;
+  color: #1f1b19;
 `;
 
 export const HotelsList: React.FC = () => {
   return (
-    <Container>
-      <HotelCard id={1} />
-      <HotelCard id={2} />
-      <HotelCard id={3} />
-      <HotelCard id={4} />
-    </Container>
+    <List>
+      <HotelCard id={1} rating={4.3} />
+      <HotelCard id={2} rating={5} />
+      <HotelCard id={3} rating={2.5} />
+      <HotelCard id={4} rating={3} />
+    </List>
   );
 };
