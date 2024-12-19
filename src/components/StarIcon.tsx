9723183450276a552eb424be93bcb51bdbeec2ed
Kate <kate@Arturs-MacBook-Pro.local>
2024-12-19 04:39:58 +0100

    feat: add reusable StarIcon component

---
+++ src/components/StarIcon.tsx
@@ -0,0 +1,13 @@
+const StarIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    viewBox="0 0 32 30"
+    fill="#FFA41C"
+  >
+    <path d="M31.77 11.857H19.74L15.99.5l-3.782 11.357H0l9.885 6.903-3.692 11.21 9.736-7.05 9.796 6.962-3.722-11.18 9.766-6.845z" />
+  </svg>
+);
+
+export default StarIcon;
