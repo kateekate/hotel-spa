2181de0280842a26a1ca3043035b6cfec98453fe
Kate <kate@Arturs-MacBook-Pro.local>
2024-12-19 03:14:52 +0100

    feat: add GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { GlobalStyles } from "./styles/GlobalStyles.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
